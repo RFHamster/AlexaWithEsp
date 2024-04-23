a1471a24a618a4559e1e7c93c69f40ab186ae62d
Rhuan Fernandes <rfcf10@gmail.com>
2024-04-22 21:30:10 -0300

    Update PortasResource.java

--- WebServerJava/src/main/java/com/robotica/bloqueadorAutomotivo/resources/PortasResource.java
+++ WebServerJava/src/main/java/com/robotica/bloqueadorAutomotivo/resources/PortasResource.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robotica.bloqueadorAutomotivo.entities.Portas;
@@ -27,9 +27,9 @@ public class PortasResource {
 		return service.savePortas(nome, tipo, porta, c);
     }
 	
-	@GetMapping("/chassi")
+	@GetMapping("/chassi/{chassi}")
     public ResponseEntity<List<Portas>> findByChassi(
-            @RequestParam("chassi") String chassi) {
+            @PathVariable("chassi") Integer chassi) {
 		Integer c = Integer.valueOf(chassi);
         List<Portas> list = service.findByIdChassi(c);
 
